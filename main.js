c30327fee3962fe21398a4fe9f7e1d86028a4e34
PopescuAndrei22 <andrei.popescu2708@gmail.com>
2023-03-16 01:46:03 +0200

    Updated main.js
    I added a restriction so that the user cannot shrink the window less than the given values

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function createWindow(){
     const win = new BrowserWindow({
         width: 500,
         height: 500,
+        minWidth: 400,
+        minHeight: 400
     });
 
     win.loadFile('src/index.html');
